dc4d39e9b4eac4cad841a20afa391f31d06a366b
Christian <chhartmann@web.de>
2024-10-28 13:40:34 +0000

    adapt linker file

--- firmware/run_on_target.sh
+++ firmware/run_on_target.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-file_path=$1
-file_name="${file_path##*/}"
+echo Run $1 on target
 sshpass -pmilkv ssh root@192.168.42.1 'echo stop > /sys/class/remoteproc/remoteproc0/state'
-sshpass -pmilkv scp -O $1 root@192.168.42.1:/lib/firmware
-sshpass -pmilkv ssh root@192.168.42.1 'echo $file_name > /sys/class/remoteproc/remoteproc0/firmware'
+sshpass -pmilkv scp -O $1 root@192.168.42.1:/lib/firmware/firmware.elf
+sshpass -pmilkv ssh root@192.168.42.1 'echo firmware.elf > /sys/class/remoteproc/remoteproc0/firmware'
 sshpass -pmilkv ssh root@192.168.42.1 'echo start > /sys/class/remoteproc/remoteproc0/state'
